20d6ccef9412e6c11946be714c5309533150e629
AZI3 <brianfounder@hotmail.com>
2020-12-26 21:39:46 -0400

    implement an example of integrating UIKit into SwiftUI following official guidance

---
+++ Landmarks/PageView/PageControl.swift
@@ -0,0 +1,42 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Brian Zhang on 2020-12-26.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        control.addTarget(context.coordinator, action: #selector(Coordinator.updateCurrentPage(sender:)), for: .valueChanged)
+        return control
+    }
+    
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc // expose code to Objective-C runtime
+        func updateCurrentPage(sender: UIPageControl) { // target-action pattern
+            control.currentPage = sender.currentPage
+        }
+    }
+}
